f077457bd29db41b12a63e06a532d670dd65d0ea
Bonus Mom <AZonaGarcia@aol.com>
2020-09-11 00:12:13 -0400

    created sed.sql file

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Truffle Burger');
+INSERT INTO burgers (name) VALUES ('Cheddar Stuffed Burger');
+INSERT INTO burgers (name, devour) VALUES ('Bacon and Kimchi Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Pimiento Cheese-Stuffed Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Sirloin Burgers with Onion Jam', true);
+INSERT INTO burgers (name) VALUES ('Italian Sausage Burger with Garlicky Spinach');
